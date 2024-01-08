7f839c24a0058203931689eb34bb0f91dcc7c55f
JavaSpringBootbyrahul <rzope5923@gmail.com>
2024-01-08 17:53:19 +0530

    ID-3443 second commit

--- src/main/java/org/muktai/JenkinsController.java
+++ src/main/java/org/muktai/JenkinsController.java
@@ -1,14 +1,14 @@
 package org.muktai;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class JenkinsController 
 {
 	@GetMapping("/")
 	public String getMessage()
 	{
-		return "<h3 style='color:red'>Welcome to jenkins pipeline</h3>";
+		return "index";
 	}
 }
